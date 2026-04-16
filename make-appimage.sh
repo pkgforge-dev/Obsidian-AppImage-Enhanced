8790dbe48400721080ffdc5a4f1ee009ea06a360
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-16 19:58:28 -0400

    this needs userns

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.hook"
+export ADD_HOOKS="self-updater.hook:fix-namespaces.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 
 # Deploy dependencies
