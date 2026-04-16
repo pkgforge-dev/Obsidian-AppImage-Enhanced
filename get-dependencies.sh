194b086b7ba3a36376a8b0e146dd1b34cc52a9f1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-16 19:55:22 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,15 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+	at-spi2-core    \
+	libnotify       \
+	libsecret       \
+	libxss          \
+	libxtst         \
+	nss             \
+	util-linux-libs \
+	xdg-utils
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -17,10 +25,21 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # If the application needs to be manually built that has to be done down here
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+case "$ARCH" in # they use AMD64 and ARM64 for the deb links
+	x86_64)  deb_arch=amd64;;
+	# Upstream does not provide aarch64 debs, but lets leave the code just in case it happens in the future
+	aarch64) deb_arch=arm64;;
+esac
+DEB_LINK=$(wget https://api.github.com/repos/obsidianmd/obsidian-releases/releases -O - \
+      | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*$deb_arch.deb")
+wget --retry-connrefused --tries=30 "$DEB_LINK" -O /tmp/app.deb
+mkdir -p ./AppDir/bin
+ar xvf /tmp/app.deb
+tar -xvf ./data.tar.xz
+mv -v ./opt/Obsidian/* ./AppDir/bin
+cp -v ./usr/share/applications/obsidian.desktop           ./AppDir
+cp -v ./usr/share/icons/hicolor/256x256/apps/obsidian.png ./AppDir
+cp -v ./usr/share/icons/hicolor/256x256/apps/obsidian.png ./AppDir/.DirIcon
+rm -rf ./data.tar.xz ./control.tar.gz ./usr ./opt
